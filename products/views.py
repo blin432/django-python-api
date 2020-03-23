1d38a3e6d026c343586f7be6930eebc8957fb1dd
benjamin lin <blin432@gmail.com>
2020-03-23 10:46:17 -0400

    django initial commit

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('hello World')
+
+
+def new(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
